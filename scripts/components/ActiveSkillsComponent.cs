9652870b543cb3a6a2a593d27b8e3dc5027646d6
Knightmare Leon <leonardjohncorpuz20@gmail.com>
2025-07-30 21:40:45 +0800

    feat(components): active skill component preloads skill buttons

--- scripts/components/ActiveSkillsComponent.cs
+++ scripts/components/ActiveSkillsComponent.cs
@@ -1,25 +1,50 @@
+using Godot.Collections;
+using Godot.Game.HSFMS.Skills;
+
 namespace Godot.Game.HSFMS.Components;
 
-[GlobalClass]  [Tool]
+[GlobalClass]
+[Tool]
 public partial class ActiveSkillsComponent : Component
 {
     private int _maxSkillPoints;
 
-    public override void _Ready()
-    {
-        base._Ready();
-    }
     private int _currentSkillPoints;
     [Export(PropertyHint.Range, "1,3,1, or_greater")]
     public int MaxSkillPoints
     {
-        get => _maxSkillPoints; 
+        get => _maxSkillPoints;
         set => _maxSkillPoints = Mathf.Clamp(value, 1, int.MaxValue);
 
     }
     public int CurrentSkillPoints
     {
-        get => _currentSkillPoints; 
+        get => _currentSkillPoints;
         set => _currentSkillPoints = Mathf.Clamp(value, 1, _maxSkillPoints);
+    }
+
+    public Button[] SkillButtons { get; private set; }
+
+
+    public override void _Ready()
+    {
+        LoadSkillButtons();
+    }
+
+    public void LoadSkillButtons()
+    {
+        SkillButtons = new Button[GetChildCount()];
+        Array<Node> children = GetChildren();
+        for (int i = 0; i < GetChildCount(); i++)
+        {
+            if (children[i] is ActiveSkill activeSkill)
+            {
+                Button skillButton = new()
+                {
+                    Icon = activeSkill.Icon
+                };
+                SkillButtons[i] = skillButton;
+            }
+        }
     }
 }
