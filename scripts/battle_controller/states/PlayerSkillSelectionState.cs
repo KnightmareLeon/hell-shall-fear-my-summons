2246783977825e30ba66f5b4a208ceed903a5503
Knightmare Leon <leonardjohncorpuz20@gmail.com>
2025-08-12 21:20:47 +0800

    feat(battle_controller): add code implementation to player skill selection state

--- scripts/battle_controller/states/PlayerSkillSelectionState.cs
+++ scripts/battle_controller/states/PlayerSkillSelectionState.cs
@@ -1,7 +1,24 @@
+using Godot.Game.HSFMS.Types;
+
 namespace Godot.Game.HSFMS;
 
 [Tool]
 
 public partial class PlayerSkillSelectionState : BattleControllerState
 {
+    [Export]
+    PlayerSkillTargetingState _playerSkillTargetingState;
+    public override State ProcessSignal(SignalType signalType, params Variant[] args)
+    {
+        switch (signalType)
+        {
+            case SignalType.ON_GETTING_SELECTED_AREA:
+
+                BattleController.SetSelectedArea((CharacterBody2D)args[0], (CharacterPlacementArea)args[1]);
+                return _playerSkillTargetingState;
+            default:
+                return null;
+        }
+    }
+
 }
