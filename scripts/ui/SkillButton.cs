ae95fea06e2fdf02ceae6364036a46177a7c0691
Knightmare Leon <leonardjohncorpuz20@gmail.com>
2025-08-21 18:51:52 +0800

    feat(skill_button): add button pressed signal

--- scripts/ui/SkillButton.cs
+++ scripts/ui/SkillButton.cs
@@ -2,7 +2,8 @@ namespace Godot.Game.HSFMS;
 
 public partial class SkillButton : Button
 {
-
+    [Signal]
+    public delegate void ButtonPressedEventHandler();
     public override void _Ready()
     {
         Connect("pressed",new Callable(this, nameof(OnButtonPressed)));
