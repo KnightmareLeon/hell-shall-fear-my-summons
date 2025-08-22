eda8a7b94726f88433c416f9ae8507405c167a82
Knightmare Leon <leonardjohncorpuz20@gmail.com>
2025-08-22 20:44:18 +0800

    feat(base_skills): change base class from node to resource

--- scripts/skills/BaseSkill.cs
+++ scripts/skills/BaseSkill.cs
@@ -3,7 +3,7 @@ using Godot.Game.HSFMS.Types;
 namespace Godot.Game.HSFMS.Skills;
 
 [GlobalClass]  [Tool]
-public abstract partial class BaseSkill : Node
+public abstract partial class BaseSkill : Resource
 {
     [Export]
     public string EffectsDescription { get; protected set; }
